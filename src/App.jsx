f68de04932761624a06966086b71f4c8861b23ec
kiran-commits <ithapetanu@gmail.com>
2024-07-10 18:12:38 +0000

    mc2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function GetRequest() {
   const [error, setError] = useState(null);
 
   const targetUrl = "https://graph.facebook.com/v20.0/me?fields=id%2Cname%2Cabout%2C&access_token=EAAQPpCDYd0YBO3Tr9EzMUgTCx5bLzazKvJjfZBA3ipr1MSeCJLTGZAb9X2ARrUWHFFhjgumeHDOOIVhJR8ydZCe8wTdlyRzvx1ZBuPNyNfVZC1C1I9soBT6RlalANvG0tuHWahoblkQbzJCUdbIBdS3gDhwrGnTpPhA4koQMBZAe0htjz9FZAKSZC9dutRdHQROMy0doEPFPDwtthKe1ZAZAIjZAJKvSwZDZD"; 
-  
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
